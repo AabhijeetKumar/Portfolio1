d40276ad70ae71f2e8a956de740d8215ff2228c7
Abhijeet Kumar <130365474+AabhijeetKumar@users.noreply.github.com>
2025-09-13 13:49:43 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,14 +45,7 @@ export const Contact = () => {
                 </h1>
             </motion.div>
 
-            {/* <div className="text-center tracking-tighter">
-
-                <motion.a
-                    whileInView={{ opacity: 1, x: 0 }}
-                    initial={{ opacity: 0, x: 100 }}
-                    transition={{ duration: 1 }}
-                    href="mailto:abhiscms01@gmail.com" className="border-b">{CONTACT.email}</motion.a>
-            </div> */}
+            
             <motion.div
                 whileInView={{ opacity: 1, y: 0 }}
                 initial={{ opacity: 0, y: 100 }}
